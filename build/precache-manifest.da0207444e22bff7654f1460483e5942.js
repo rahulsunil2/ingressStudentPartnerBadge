db250536aa54e070f0086da2e66c95846fd12eaf
Rahul Sunil <rahulsunil2@gmail.com>
2021-02-07 20:22:10 +0530

    Mentor

---
+++ build/precache-manifest.da0207444e22bff7654f1460483e5942.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087be3f90ff9ee4a199d743820540c70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c42e25a742bb770195fa",
+    "url": "./static/css/2.78c3f47d.chunk.css"
+  },
+  {
+    "revision": "4e00edab091424cf54ec",
+    "url": "./static/css/main.090a3684.chunk.css"
+  },
+  {
+    "revision": "c42e25a742bb770195fa",
+    "url": "./static/js/2.acbff713.chunk.js"
+  },
+  {
+    "revision": "611eb6586a38a1198fe7a7e94fdf6e58",
+    "url": "./static/js/2.acbff713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e00edab091424cf54ec",
+    "url": "./static/js/main.d171fcc6.chunk.js"
+  },
+  {
+    "revision": "29d00fcca067495c072c",
+    "url": "./static/js/runtime-main.23f2cfe0.js"
+  },
+  {
+    "revision": "86875030a0f2ce2dbcba821f33dbcd18",
+    "url": "./static/media/frame-light.86875030.png"
+  }
+]);
